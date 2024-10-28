d202cb64c175e6dff5d0fc5de417cf3a24f6c68f
morita <99958533+Mor1Sh0@users.noreply.github.com>
2024-10-28 15:57:09 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,6 +0,0 @@
-const b = document.getElementById('btn');
-
-// HTML要素がクリックされたときにイベント処理を実行する
-b.addEventListener('click', () => {
-  document.getElementById('text').textContent = 'ボタンをクリックしました';
-});
